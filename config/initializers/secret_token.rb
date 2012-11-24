ba66a14ea352613899a1346e7e0774e2c824e9d8
Hirotaka Muranaka <muranaka.hirotaka@O-02619-MAC.local>
2012-11-24 13:37:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FootballBakazou::Application.config.secret_token = 'e14993da75bd17aecab5424697c9d0884055f44c7a66d6cfb6502b4cc6275b330a23ed87b90cdd6ee47edf1a0e4af817c0cd11613a328f683e9c9cfcb022aa5f'
